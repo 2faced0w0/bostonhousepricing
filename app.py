4a759f674c6187b598d95a2dd969d7e7b4691023
Ritwick Roy <ritwickroy2000@gmail.com>
2023-03-12 21:14:33 +0530

    made changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/predict_api', methods=['POST'])
 
